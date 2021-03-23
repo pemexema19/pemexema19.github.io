1cf3b7db821765f81fc7e23a465e58222c33e00a
pemexema19 <81199648+pemexema19@users.noreply.github.com>
2021-03-23 10:33:53 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
-    authDomain: "pauten43.firebaseapp.com",
-    projectId: "pauten43",
-    storageBucket: "pauten43.appspot.com",
-    messagingSenderId: "167024006136",
-    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
-    measurementId: "G-S4KD63PHHW"
+    apiKey: "AIzaSyBi5Yx-NVhKdKeqR1qE83VFr7dzZmtrzws",
+    authDomain: "autentifica41ecl.firebaseapp.com",
+    projectId: "autentifica41ecl",
+    storageBucket: "autentifica41ecl.appspot.com",
+    messagingSenderId: "758364802517",
+    appId: "1:758364802517:web:ae6ae6075d19fbec74415c",
+    measurementId: "G-NWN0GSVPLS"
     });
